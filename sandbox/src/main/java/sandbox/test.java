2acc4790cc67e223c48bc0e018cc1314855eabf6
HUNGI KE <HUNGI KE>
2017-07-04 20:11:53 -0500

    update test.java in the project sandbox.

--- sandbox/src/main/java/sandbox/test.java
+++ sandbox/src/main/java/sandbox/test.java
@@ -23,11 +23,14 @@ import org.neuroph.nnet.Perceptron;
 
 public class test {
 	
-	public NeuralNetwork neuralNetwork = new MultiLayerPerceptron(2,3,1);
+	public NeuralNetwork neuralNetwork;
 	
+	public test( NeuralNetwork neuralNetwork ){
+		this.neuralNetwork=neuralNetwork;
+	}
 	
 	private double estimatedFunc(double x1,double x2){
-		// return x1+x2;
+		//   return x1+x2;
 		return Math.pow(x1,3)+Math.pow(x2,2)+x1;
 	}
 	
@@ -69,7 +72,7 @@ public class test {
 		for(int i=0;i<2000000;i++){
 			//double x1=(int)(i/100),x2=i%100;
 			double x1=Math.random()*100,x2=Math.random()*100;
-			double y=estimatedFunc(x1,x2)/1010100;
+			double y=estimatedFunc(x1,x2)/OUTPUT_UPPERBOUND;
 			trainingSet.addRow(new DataSetRow( intputNormailize( x1,x2 ),new double[]{ y }));
 		
 		}
@@ -100,7 +103,7 @@ public class test {
 		.build();
 		
 		
-		Genotype<DoubleGene> result = engine.stream().limit(3000).collect(EvolutionResult.toBestGenotype());
+		Genotype<DoubleGene> result = engine.stream().limit(1000).collect(EvolutionResult.toBestGenotype());
 	 
 	    System.out.println("Result:\n" + result);
 
@@ -118,8 +121,10 @@ public class test {
 		
 	}
 	
+	
+	private final double OUTPUT_UPPERBOUND=estimatedFunc(100d,100d);
 	private double outputNormalize(double output){
-		return output*1010100;
+		return output*OUTPUT_UPPERBOUND;
 	}
 	
 	
@@ -129,7 +134,7 @@ public class test {
 	
 	public void estimate(){
 
-		
+		int bias_avg=0;
 		
 	    for(int i=0;i<=10000;i++){
 			double x1=Math.random()*100,x2=Math.random()*100;
@@ -145,24 +150,24 @@ public class test {
 	    	System.out.println("x1="+x1+" , x2="+x2);
 	    	System.out.println("output="+ output +",eval func="+y1);
 	    	System.out.println("bias="+bias);
-	    	
+	    	bias_avg+=Math.abs(bias);
 	    	
 	    	System.out.println();
 	    	System.out.println();
 	    }
 		
-		
+	    System.out.println("bias avg="+(bias_avg/10000) );
 	}
 	
 	
 
 	public static void main(String[] args) {
-		test test=new test();
+		test test=new test( new MultiLayerPerceptron(2,3,3,1));
 		
-		test.taringByGA();
+		// test.taringByGA();
 		
-		// test.neuralNetwork.randomizeWeights();
-		// test.taringByBP();
+		test.neuralNetwork.randomizeWeights();
+		test.taringByBP();
 		
 		test.estimate();
 		
